83e837f33443cadf74c845af1ed8bef9e40869ec
Neurotok <marcin@pingpongcat.com>
2020-04-21 17:16:39 +0200

    footer

--- lib/about.dart
+++ lib/about.dart
@@ -17,22 +17,23 @@ class AboutMe extends StatelessWidget {
           flex: 2,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
+            //crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               SizedBox(
                 height: 100,
               ),
               Text(
                 "About me",
-                  style: GoogleFonts.didactGothic(
+                style: GoogleFonts.didactGothic(
                   fontSize: 32,
                   color: Color(0xFFFFFFFF),
                   letterSpacing: 2.0,
                   fontWeight: FontWeight.w100,
-                  
                 ),
               ),
-              SizedBox(height: 20,),
+              SizedBox(
+                height: 20,
+              ),
               Text(
                 "Hello, I'm Sam. I'm a theme for Hugo. I'm an extremely customizable, content-focused theme that lets you showcase your work your way. I have slick minimalist templates for list pages (such as a list of blog posts) and single pages (a page to read one blog post). You can use tags on your content, and view content by tags. I also have a simple responsive gallery built with CSS grid. It'll look great on any screen, and all you have to do is provide a folder of images. I'm built and maintained by Victoria Drake. Here's her GitHub. This page uses the default single.html template.m",
                 style: GoogleFonts.didactGothic(
@@ -42,73 +43,90 @@ class AboutMe extends StatelessWidget {
                   fontWeight: FontWeight.w100,
                 ),
               ),
-              SizedBox(height: 20,),
-              Divider(
-                color: Color(0x33FFFFFF),
-                thickness: 1.0,
+              SizedBox(
+                height: 20,
               ),
-              Row(children: <Widget>[
-                FlatButton(
-                    splashColor: Colors.transparent,
-                    highlightColor: Colors.transparent,
-                    hoverColor: Colors.transparent,
-                    padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
-                    onPressed: () {
-                      Navigator.pushReplacement(context, 
-                      MaterialPageRoute(builder: (context) => Portfolio())
-                      );
-                    },
-                    child: Text(
-                      "porfolio",
-                      style: GoogleFonts.didactGothic(
-                        fontSize: 16,
-                        color: Color(0xFFCCCCCC),
-                        letterSpacing: 2.0,
-                        fontWeight: FontWeight.w100,
-                      ),
-                    ),
-                  ),
-                FlatButton(
-                    splashColor: Colors.transparent,
-                    highlightColor: Colors.transparent,
-                    hoverColor: Colors.transparent,
-                    padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
-                    onPressed: () {
-                      Navigator.pushReplacement(context, 
-                      MaterialPageRoute(builder: (context) => Contact())
-                      );
-                    },
-                    child: Text(
-                      "contact",
-                      style: GoogleFonts.didactGothic(
-                        fontSize: 16,
-                        color: Color(0xFFCCCCCC),
-                        letterSpacing: 2.0,
-                        fontWeight: FontWeight.w100,
-                      ),
-                    ),
-                  ),
-                  FlatButton(
-                    splashColor: Colors.transparent,
-                    highlightColor: Colors.transparent,
-                    hoverColor: Colors.transparent,
-                    padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
-                    onPressed: () {
+              Container(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                decoration: BoxDecoration(
+                      border: Border(
 
-                      Navigator.pop(context);
-                    },
-                    child: Text(
-                      "home",
-                      style: GoogleFonts.didactGothic(
-                        fontSize: 16,
-                        color: Color(0xFFCCCCCC),
-                        letterSpacing: 2.0,
-                        fontWeight: FontWeight.w100,
+                          top: BorderSide(
+
+                              color: Color(0xFFCCCCCC), width: 1.0))),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FlatButton(
+                      splashColor: Colors.transparent,
+                      highlightColor: Colors.transparent,
+                      hoverColor: Colors.transparent,
+                      padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      onPressed: () {
+                        Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder: (context) => Portfolio()));
+                      },
+                      child: Text(
+                        "porfolio",
+                        style: GoogleFonts.didactGothic(
+                          fontSize: 16,
+                          color: Color(0xFFCCCCCC),
+                          letterSpacing: 2.0,
+                          fontWeight: FontWeight.w100,
+                        ),
                       ),
                     ),
-                  ),
-              ],),
-              Text("Ping pong cat 2020"),
+                    FlatButton(
+                      splashColor: Colors.transparent,
+                      highlightColor: Colors.transparent,
+                      hoverColor: Colors.transparent,
+                      padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      onPressed: () {
+                        Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder: (context) => Contact()));
+                      },
+                      child: Text(
+                        "contact",
+                        style: GoogleFonts.didactGothic(
+                          fontSize: 16,
+                          color: Color(0xFFCCCCCC),
+                          letterSpacing: 2.0,
+                          fontWeight: FontWeight.w100,
+                        ),
+                      ),
+                    ),
+                    FlatButton(
+                      splashColor: Colors.transparent,
+                      highlightColor: Colors.transparent,
+                      hoverColor: Colors.transparent,
+                      padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text(
+                        "home",
+                        style: GoogleFonts.didactGothic(
+                          fontSize: 16,
+                          color: Color(0xFFCCCCCC),
+                          letterSpacing: 2.0,
+                          fontWeight: FontWeight.w100,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 30,),
+              Text(
+                "Ping Pong Cat 2020",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.didactGothic(
+                  fontSize: 15,
+                  color: Color(0xFFCCCCCC),
+                  letterSpacing: 1.0,
+                  fontWeight: FontWeight.w100,
+                ),
+              ),
             ],
           ),
         ),
