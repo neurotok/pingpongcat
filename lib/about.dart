528944df7e7209664cd9f74d59c871d7b63ef681
Neurotok <marcin@pingpongcat.com>
2020-04-23 21:13:33 +0200

    typography

--- lib/about.dart
+++ lib/about.dart
@@ -4,6 +4,11 @@ import 'package:pingpongcat/contact.dart';
 import 'package:pingpongcat/portfolio.dart';
 
 class AboutMe extends StatelessWidget {
+
+  //TODO
+  //var _year = DateTime.now();
+  //final String _yearStr = _yearStr.
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -17,7 +22,7 @@ class AboutMe extends StatelessWidget {
           flex: 2,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            //crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               SizedBox(
                 height: 100,
@@ -29,64 +34,67 @@ class AboutMe extends StatelessWidget {
               SizedBox(
                 height: 20,
               ),
-              RichText(text: TextSpan( style: Theme.of(context).textTheme.bodyText1, children: [
-                  TextSpan(text:"My name is Marcin Zając. I graphics designer and developer from Poland with several years of experience in graphics user interfaces.\n\nCombining technology with excellent visual experience became my passion. In programming I strives for the highest standards of software engineering and graphic design gives me a vent for the artistic soul"),
-                  TextSpan(text:"My skill set:"),
-                  TextSpan(text:"SDL2",),
-                  
-              ]),),
+              Text("My name is Marcin Zając. I graphics designer and developer from Poland with several years of experience in graphics user interfaces.\n\nCombining technology with excellent visual experience became my passion. In programming I strives for the highest standards of software engineering and graphic design gives me a vent for the artistic soul\n\n", style: Theme.of(context).textTheme.bodyText1,),
+              Text("My skills set:", style: Theme.of(context).textTheme.headline4,),
+              SizedBox(height: 20,),
               SizedBox(
-                height: 20,
+                height: 25,
               ),
-              Container(
-                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                decoration: BoxDecoration(
-                    border: Border(
-                        top: BorderSide(color: Color(0xFFCCCCCC), width: 1.0))),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    FlatButton(
-                      splashColor: Colors.transparent,
-                      highlightColor: Colors.transparent,
-                      hoverColor: Colors.transparent,
-                      padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => Portfolio()));
-                      },
-                      child: Text("porfolio", style: Theme.of(context).textTheme.headline5,),
-                    ),
-                    FlatButton(
-                      splashColor: Colors.transparent,
-                      highlightColor: Colors.transparent,
-                      hoverColor: Colors.transparent,
-                      padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
-                      onPressed: () {
-                        Navigator.pushReplacement(context,
-                            MaterialPageRoute(builder: (context) => Contact()));
-                      },
-                      child: Text("contact", style: Theme.of(context).textTheme.headline5,),
-                    ),
-                    FlatButton(
-                      splashColor: Colors.transparent,
-                      highlightColor: Colors.transparent,
-                      hoverColor: Colors.transparent,
-                      padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text("home", style: Theme.of(context).textTheme.headline5,),
-                    ),
-                  ],
+              Center(
+                child: Container(
+                  width: 300,
+                  decoration: BoxDecoration(
+                      border: Border(
+                          top: BorderSide(color: Color(0xFFCCCCCC), width: 1.0))),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      FlatButton(
+                        splashColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Portfolio()));
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(border: Border(bottom: BorderSide(width: 1.0, color: Color(0xFFFFFFFF)))),
+                          child: Text("porfolio", style: Theme.of(context).textTheme.headline5,),
+                        ),
+                      ),
+                      Text("\u2022"),
+                      FlatButton(
+                        splashColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                        onPressed: () {
+                          Navigator.pushReplacement(context,
+                              MaterialPageRoute(builder: (context) => Contact()));
+                        },
+                        child: Container(decoration: BoxDecoration(border:Border(bottom: BorderSide(width: 1.0, color: Color(0xFFFFFFFF)))),
+                          child: Text("contact", style: Theme.of(context).textTheme.headline5,)),
+                      ),
+                      Text("\u2022"),
+                      FlatButton(
+                        splashColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Container(decoration: BoxDecoration(border:Border(bottom: BorderSide(width: 1.0, color: Color(0xFFFFFFFF)))),
+                        child: Text("home", style: Theme.of(context).textTheme.headline5,)),
+                      ),
+                    ],
+                  ),
                 ),
               ),
               SizedBox(
-                height: 30,
+                height: 25,
               ),
-              Text("Ping Pong Cat 2020", textAlign: TextAlign.center, style: Theme.of(context).textTheme.headline6,),
+              Center(child: Text("Ping Pong Cat 2020", textAlign: TextAlign.center, style: Theme.of(context).textTheme.headline6,)),
             ],
           ),
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,10 @@ class PingPongCat extends StatelessWidget {
         textTheme: TextTheme(
           headline2: TextStyle(fontSize: 48, color: Color(0xFFCCCCCC), letterSpacing: 2.0, fontWeight: FontWeight.w100,),
           headline3: TextStyle( fontSize: 32, color: Color(0xFFFFFFFF), letterSpacing: 2.0, fontWeight: FontWeight.w100,),
+          headline4: TextStyle(fontSize: 18, color: Color(0xFFCCCCCC), letterSpacing: 2.0, fontWeight: FontWeight.bold,),
           headline5: TextStyle(fontSize: 16, color: Color(0xFFCCCCCC), letterSpacing: 2.0, fontWeight: FontWeight.w100,),
           headline6: TextStyle(fontSize: 15, color: Color(0xFFCCCCCC), letterSpacing: 1.0, fontWeight: FontWeight.w100,),
           bodyText1: TextStyle(fontSize: 16.0, color: Color(0xFFCCCCCC), letterSpacing: 2.0, fontWeight: FontWeight.w100,),
-          
         ),
       ),
       title: "Ping Pong Cat",
